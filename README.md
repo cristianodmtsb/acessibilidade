5f9e9a71763339b2e10d5dc6dd8bbd992498cfac
Cristiano Sigep <cristiano.castro@sigep.com.br>
2018-06-08 15:24:01 -0300

    Configurações Básicas do JS

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # acessibilidade
 Código super simples em JavaScript puro para acessibilidade.
+
+Com o intuito de ajudar a quem precisa criei essa classe super simples em JavaScript puro para ser usada em qualquer projeto WEB. 
+
+<h3>Como usar</h3>
+
+<b>1. Inicie os Arquivos</b>
+
+Para começar a usar, inicie os arquivos CSS e JS.
+
+```html
+<head>
+    <title>Acessibilidade</title>
+
+    <!-- STYLE E CONTRASTE -->
+    <link rel='stylesheet' id='styleContraste' href="css/style.css"  type='text/css' media='all' />
+
+    <!-- ACESSIBILIDADE JS -->
+    <script src="js/acessibilidade.js"></script>
+</head>
+```
+
+OBS: Use o ID para poder mudar o CSS que será alterado. 
+
+DICA: Para não precisar escrever duas folhas de estilo, geralmente uso dois css um abaixo do outro. Sendo o primeiro o CSS DEFAULT e o segundo o css de contraste sobrescrevendo apenas as cores do arquivo DEFAULT. 
+
+<b>2. Inicie a Função de Cookie</b>
+
+Para que os textos e cores sejam sempre ajustados conforme o salvo no COOKIE.
+
+```html
+<body onload="checkCookie()">
+```
+
+<b>3. Adicione os Botões de Fonte</b>
+
+Para que as fontes possam ser alteradas e salvas no COOKIE utilize a base abaixo. Você pod estilizar da forma que quiser.
+
+```html
+<span class="font-con">
+    <button Onclick="mudaFonte('menos'); return false;">A-</button>
+    <button Onclick="mudaFonte('padrao'); return false;">A</button>
+    <button Onclick="mudaFonte('mais'); return false;">A+</button>
+</span>
+```
+
+<b>4. Adicione o Botão de Contraste</b>
+
+Ultilize o BUTTON abaixo para mudar o estilo css para o contraste. Para que altera a LABEL do botão use o ID.
+
+```html
+<span class="font-con">
+    <button id="mudaEstilo" onClick="mudaContraste()">PRETO</button>
+</span>
+```
+
+<b>5. Adicione a Classe de Formatação</b>
+
+Para poder aumentar e diminuir suas fontes use a classe "txtFontC", ou altere no arquivo JS para a que melhor lhe atender. 
+
+```html
+<div class="txtFontC">
+    <p>Lorem ipsum dolor sit amet...</p>
+    <p>Lorem ipsum dolor sit, amet...</p>
+</div>
+```
+
+<h3>Conclusão</h3>
+
+Veja abaixo alguns sites que usam esses mesmo códigos:
+
+https://www.senadorcanedo.go.gov.br/
+https://www.altohorizonte.go.gov.br/
+http://www.perolandia.go.gov.br/
--- js/acessibilidade.js
+++ js/acessibilidade.js
@@ -3,18 +3,21 @@ var tam = 1.0
 
 //MUDA TAMANHO DA FONTE E CRIA O COOKIE COM A INFORMAÇÃO DE TAMANHO
 function mudaFonte(tipo,elemento) {
+
+    //VERIFICA TAMANHO DA FONTE PARA ALTERAR O TAMANHO
     if (tipo=="mais") {
-        if(tam<2.0)tam+=0.1;
+        if(tam<1.5)tam+=0.1 //AJUSTE DE TAMANHO MÁXIMO DE FONTE
         createCookie("fonte",tam,365)
     } else if(tipo=="menos") {
-        if(tam>0.5) tam -=0.1
+        if(tam>0.5) tam -=0.1 //AJUSTE DE TAMANHO MINIMO DE FONTE
         createCookie("fonte",tam,365)
     } else {
         if(tam>0.95 || tam<1.1) tam = 1.0
         createCookie("fonte", tam,365)
     }
 
-    var list = document.body.querySelectorAll(".txtFontC")
+    //VERIFICA QUANTOS ELEMENTOS POSSUEM A CLASSE DE FORMATAÇÃO DE TAMANHO
+    var list = document.body.querySelectorAll(".txtFontC") //CLASSE DE ALTERAÇÃO DE FONTE
 	for (var i = 0; i < list.length; ++i) {
         var item = list[i]
         item.style.fontSize = tam+"rem"
@@ -24,30 +27,31 @@ function mudaFonte(tipo,elemento) {
 
 //CRIA A ULR BASE DO CSS E MUDA O CSS NO HREF
 function mudaStyleSheet(sheet) {
-    var baseUrl = window.location.origin + '/wp-content/themes/Sigep-base/library/css/'
+    var baseUrl = window.location.origin + '/javascript/acessibilidade/css/' //AJUSTAR URL PADRÃO DA PASTA CSS
     var styleUrl = baseUrl + sheet
     document.getElementById("styleContraste").setAttribute('href', styleUrl)
 }
 
 //CRIA O COOKIE DA FOLHA DE ESTILO DO CONTRASTE E INSERE O ICONE NO BOTÃO
 function mudaContraste() {
-    var btnStyle = document.getElementById('mudaEstilo')
-    var btnActive = btnStyle.classList.contains('active')
+    var btnStyle = document.getElementById('mudaEstilo') //ID DO BOTÃO DE MUDANÇA DE CONTRASTE
+    var btnActive = btnStyle.classList.contains('active') //IDENTIFICA CLASSE ACTIVE DO BOTÃO
 
     if (btnActive) {
-        btnStyle.className = ''
-        btnStyle.innerHTML = '<i class="material-icons icontraste">brightness_6</i>'
-        mudaStyleSheet('styles.css')
-        createCookie("styleSheet", 'styles.css',365)
+        //FORMATAÇÃO DO BOTÃO, MUDANÇA DO CSS E CRIAÇÃO DO COOKIE DE CSS
+        btnStyle.className = 'btn btn-dark btn-sm'
+        btnStyle.innerHTML = 'PRETO'
+        mudaStyleSheet('style.css')
+        createCookie("styleSheet", 'style.css',365)
     } else {
-        btnStyle.className = 'active'
-        btnStyle.innerHTML = '<i class="material-icons icontraste">brightness_high</i>'
+        //FORMATAÇÃO DO BOTÃO, MUDANÇA DO CSS E CRIAÇÃO DO COOKIE DE CSS
+        btnStyle.className = 'active btn btn-light btn-sm'
+        btnStyle.innerHTML = 'BRANCO'
         mudaStyleSheet('contraste.css')
         createCookie("styleSheet", 'contraste.css',365)
     }
 
 }
-
 
 //SALVA O COOKIE COM UM PERIODO DETERMINADO DE TEMPO
 function createCookie(name,value,days){
@@ -91,13 +95,12 @@ function checkCookie() {
         var btnActive = btnStyle.classList.contains('active')
 
         if (contrasteCookie === 'contraste.css') {
-            btnStyle.className = 'active'
-            btnStyle.innerHTML = '<i class="material-icons icontraste">brightness_high</i>'
+            btnStyle.className = 'active btn btn-light btn-sm'
+            btnStyle.innerHTML = 'BRANCO'
             mudaStyleSheet('contraste.css')
-        } else if(contrasteCookie === 'styles.css'){
-            btnStyle.className = ''
-            btnStyle.innerHTML = '<i class="material-icons icontraste">brightness_6</i>'
-            mudaStyleSheet('styles.css')
+        } else if(contrasteCookie === 'style.css'){
+            btnStyle.className = 'btn btn-dark btn-sm'
+            btnStyle.innerHTML = 'PRETO'
         }else {
             console.log('nada')
         }
